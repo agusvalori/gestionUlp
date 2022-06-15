bebdad6b393c1a289dad7f910771f24941288268
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-06-15 17:31:24 -0300

    agregando libreria para conectar mariadb 3.0.5

--- src/main/java/entidades/Inscripcion.java
+++ src/main/java/entidades/Inscripcion.java
@@ -1,14 +1,13 @@
-package modelo;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package entidades;
 
 /**
  *
  * @author agusv
  */
-public class Alumno {
+public class Inscripcion {
     
 }
